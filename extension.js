e487570ec6395b7d56920b34319ea681700f610e
bentx <bentx@users.noreply.github.com>
2016-12-07 11:52:53 +0700

    set activation back to onCommand, add reset

--- extension.js
+++ extension.js
@@ -4,17 +4,20 @@ const vscode = require('vscode')
 const theme = require('./lib/theme')
 
 function activate(context) {
-    theme.register()
     const apply = vscode.commands.registerCommand('theme.apply', function () {
+        theme.register()
         theme.apply()
     })
     context.subscriptions.push(apply)
+    const reset = vscode.commands.registerCommand('theme.reset', function () {
+        theme.reset()
+    })
+    context.subscriptions.push(reset)
 }
 
 exports.activate = activate
 
 function deactivate() {
-    theme.reset()
 }
 
 exports.deactivate = deactivate
