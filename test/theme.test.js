c79a72ff5c4fe64ae06e321e378f49e3df2a3d96
bentx <bentx@users.noreply.github.com>
2016-12-14 17:03:06 +0700

    upgrade outdated dependencies

--- test/theme.test.js
+++ test/theme.test.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const mocha = require('mocha')
 const assert = require('chai').assert
 const rewire = require('rewire')
 const theme = rewire('../lib/theme')
@@ -10,30 +9,30 @@ const fs = require('fs')
 const js = fs.readFileSync(theme.__get__('jsPath'), config.encoding)
 const css = fs.readFileSync(theme.__get__('cssPath'), config.encoding)
 
-mocha.suite('Extension Tests', function () {
-    mocha.test('Integrity check can be patched', function () {
+suite('Extension Tests', function () {
+    test('Integrity check can be patched', function () {
         assert.include([
             js.includes(config.regex.integrity.on),
             js.includes(config.regex.integrity.off)
         ], true, 'Workbench js contains integrity check')
     })
-    mocha.test('Activity bar can be themed', function () {
+    test('Activity bar can be themed', function () {
         assert.equal(before(), 0, 'Workbench css does not contains the patched color')
         assert.equal(after('_setActivitybar'), 2, 'Workbench css contains 2 elements with the patched color')
     })
-    mocha.test('Filetree can be themed', function () {
+    test('Filetree can be themed', function () {
         assert.equal(before(), 0, 'Workbench css does not contains the patched color')
         assert.equal(after('_setFiletree'), 4, 'Workbench css contains 4 elements with the patched color')
     })
-    mocha.test('Tabs can be themed', function () {
+    test('Tabs can be themed', function () {
         assert.equal(before(), 0, 'Workbench css does not contains the patched color')
         assert.equal(after('_setTabs'), 10, 'Workbench css contains 10 elements with the patched color')
     })
-    mocha.test('Status bar can be themed', function () {
+    test('Status bar can be themed', function () {
         assert.equal(before(), 0, 'Workbench css does not contains the patched color')
         assert.equal(after('_setStatusbar'), 4, 'Workbench css contains 4 elements with the patched color')
     })
-    mocha.test('Feedback smiley can be patched', function () {
+    test('Feedback smiley can be patched', function () {
         assert.include([
             css.includes(config.regex.statusbar.smiley.on),
             css.includes(config.regex.statusbar.smiley.off)
