19031c6ad6d4a845fcea0d6fbb74fd7f84f3950d
bentx <bentx@users.noreply.github.com>
2017-02-05 20:43:41 +0700

    add ts extension test

--- test/theme.test.js
+++ test/theme.test.js
@@ -8,6 +8,7 @@ const config = theme.__get__('config')
 const fs = require('fs')
 const js = fs.readFileSync(theme.__get__('jsPath'), config.encoding)
 const css = fs.readFileSync(theme.__get__('cssPath'), config.encoding)
+const ts = fs.readFileSync(theme.__get__('tsPath'), config.encoding)
 
 suite('Extension Tests', function () {
     test('Integrity check can be patched', function () {
@@ -17,20 +18,20 @@ suite('Extension Tests', function () {
         ], true, 'Workbench js contains integrity check')
     })
     test('Activity bar can be themed', function () {
-        assert.equal(before(), 0, 'Workbench css does not contains the patched color')
-        assert.equal(after('_setActivitybar'), 2, 'Workbench css contains 2 elements with the patched color')
+        assert.equal(before(css), 0, 'Workbench css does not contains the patched color')
+        assert.equal(after(css, '_setActivitybar'), 2, 'Workbench css contains 2 elements with the patched color')
     })
     test('Filetree can be themed', function () {
-        assert.equal(before(), 0, 'Workbench css does not contains the patched color')
-        assert.equal(after('_setFiletree'), 4, 'Workbench css contains 4 elements with the patched color')
+        assert.equal(before(css), 0, 'Workbench css does not contains the patched color')
+        assert.equal(after(css, '_setFiletree'), 4, 'Workbench css contains 4 elements with the patched color')
     })
     test('Tabs can be themed', function () {
-        assert.equal(before(), 0, 'Workbench css does not contains the patched color')
-        assert.equal(after('_setTabs'), 8, 'Workbench css contains 8 elements with the patched color')
+        assert.equal(before(css), 0, 'Workbench css does not contains the patched color')
+        assert.equal(after(css, '_setTabs'), 8, 'Workbench css contains 8 elements with the patched color')
     })
     test('Status bar can be themed', function () {
-        assert.equal(before(), 0, 'Workbench css does not contains the patched color')
-        assert.equal(after('_setStatusbar'), 9, 'Workbench css contains 9 elements with the patched color')
+        assert.equal(before(css), 0, 'Workbench css does not contains the patched color')
+        assert.equal(after(css, '_setStatusbar'), 9, 'Workbench css contains 9 elements with the patched color')
     })
     test('Feedback smiley can be patched', function () {
         assert.include([
@@ -38,13 +39,17 @@ suite('Extension Tests', function () {
             css.includes(config.regex.statusbar.smiley.off)
         ], true, 'Workbench css contains feedback smiley')
     })
+    test('TypeScript extension can be themed', function () {
+        assert.equal(before(ts), 0, 'TypeScript extension does not contains the patched color')
+        assert.equal(after(ts, '_setTs'), 1, 'TypeScript extension contains 1 element with the patched color')
+    })
 })
 
-function before() {
-    return (css.match(new RegExp(fixtures.color, 'g')) || []).length
+function before(target) {
+    return (target.match(new RegExp(fixtures.color, 'g')) || []).length
 }
 
-function after(method) {
-    const themed = theme.__get__(method)(fixtures.settings, css)
+function after(target, method) {
+    const themed = theme.__get__(method)(fixtures.settings, target)
     return (themed.match(new RegExp(fixtures.color, 'g')) || []).length
 }
